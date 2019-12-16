ae3d17f5d7961176158afaedbaa6ec98206930fa
Sayan Chaudhry <sayan.chaudhry@gmail.com>
2019-12-16 04:31:21 -0500

    age working

--- backend/api.py
+++ backend/api.py
@@ -3,6 +3,8 @@ from flask_restful import Api, Resource, reqparse
 
 import json
 
+from gad import getAge
+
 app = Flask(__name__)
 api = Api(app)
 
@@ -10,9 +12,10 @@ class Res(Resource):
 
     def post(self):
         image = request.get_data()
+        age = getAge(image)
         response = jsonify(
             ethnicity='white',
-            age=21
+            age=age
         )
         response.headers['Access-Control-Allow-Origin'] = '*'
         response.headers['Access-Control-Allow-Credentials'] = True
@@ -20,3 +23,5 @@ class Res(Resource):
 
 api.add_resource(Res, "/ai")
 app.run(host='0.0.0.0', debug=True) # runs on port 5000 by default
+
+
